acb2dfe23a6e184204c89407400121ca478be4ab
thiagomedinad21@gmail.com <thiagomedinad21@gmail.com>
2023-04-03 09:34:20 -0300

    changes

--- ClientMulticast.java
+++ ClientMulticast.java
@@ -6,40 +6,48 @@ import java.net.MulticastSocket;
 import java.net.NetworkInterface;
 
 public class ClientMulticast {
-   public static void main(String[] args) throws IOException {	   
-	      
-	      String msg = " ";	      
+	public static void main(String[] args) throws IOException {
 
-				// Duas portas diferentes, uma para entretenimento e outra para esportes
-	      MulticastSocket socket = new MulticastSocket(4321);
-	      
-	      InetAddress ia = InetAddress.getByName("230.0.0.0");
+		int PORT_ESPORTE = 22622;
+		int PORT_ENTRETENIMENTO = 66266;
 
-				// Um grupo para entretenimento e outro para esportes
-	      InetSocketAddress grupo = new InetSocketAddress(ia , 4321);
+		String msg = " ";
 
-	      NetworkInterface ni = NetworkInterface.getByInetAddress(ia);
+		// Duas portas diferentes, uma para entretenimento e outra para esportes
+		MulticastSocket socket = new MulticastSocket(4321);
 
-				// If para determinar o socket escolhido pelo cliente. Entretenimento ou Esportes
-	      socket.joinGroup(grupo, ni);
-	      
-	      while(!msg.contains("Servidor Encerrado!")){
-	         System.out.println("[Cliente] Esperando por mensagem Multicast...");
-	         byte[] buffer = new byte[1024];
-	         
-	         DatagramPacket packet=new DatagramPacket(buffer,buffer.length);
-	         socket.receive(packet);
-	         
-	         msg = new String(packet.getData());
-	         System.out.println("[Cliente] Mensagem recebida do Servidor: "+msg);
-	       
-	      }
+		InetAddress ia = InetAddress.getByName("230.0.0.0");
 
-          System.out.println("[Cliente] Conexao Encerrada!");
-	      socket.leaveGroup(grupo, ni);
-	      socket.close();
-	   
-   }
+		// Um grupo para entretenimento e outro para esportes
+		InetSocketAddress grupo = new InetSocketAddress(ia, 4321);
 
-  
+		NetworkInterface ni = NetworkInterface.getByInetAddress(ia);
+
+		// If para determinar o socket escolhido pelo cliente. Entretenimento ou Esportes
+		socket.joinGroup(grupo, ni);
+
+		// Escolher o nome de usuário
+
+		while (!msg.equals("SAIR")) {
+			// System.out.println("[Cliente] Esperando por mensagem Multicast...");
+			System.out.print("[Servidor] Digite a mensagem: ");
+
+			byte[] envio = new byte[1024];
+			
+ 			// If para determinar o socket escolhido pelo cliente. Entretenimento ou Esportes
+
+			DatagramPacket packet = new DatagramPacket(envio, envio.length, PORT);
+			socket.receive(packet);
+
+			msg = new String(packet.getData());
+			System.out.println("[Cliente] Mensagem recebida do Servidor: " + msg);
+
+		}
+
+		System.out.println("[Cliente] Conexao Encerrada!");
+		socket.leaveGroup(grupo, ni);
+		socket.close();
+
+	}
+
 }
